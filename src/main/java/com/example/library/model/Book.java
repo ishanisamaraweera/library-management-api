0b1ac1f50b8e2b83d410de7cd707ebdee33076c8
IshaniSamaraweera <ishanisamaraweera@gmail.com>
2026-01-09 00:18:30 +0530

    Issue fix

--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -1,5 +1,6 @@
 package com.example.library.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
